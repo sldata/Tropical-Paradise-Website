2b2140a084f532b92042c97c5fb013c4d63856b2
Slawek01~ <lilcrunker@gmail.com>
2022-09-25 13:32:25 +0200

    added animation on navigation

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,26 @@
 const nav = document.querySelector('.nav')
 const burgerBtn = document.querySelector('.burger-btn')
 const navItems = document.querySelectorAll('.nav__item')
+const navItemsAnimaton = document.querySelector('.nav-items-animaton')
 
 const handleNav = () => {
 	nav.classList.toggle('nav--active') 
+     
      navItems.forEach((item)=>{
           item.addEventListener('click', ()=>{
-               nav.classList.remove ('nav--active')
+             nav.classList.remove ('nav--active')
           })
      })
+     handleNavItemsAnimation()
 }
+const handleNavItemsAnimation=()=>{
+     let delayTime = 0;
+
+     navItems.forEach(item =>{
+          item.classList.toggle('nav-items-animaton')
+          item.style.animationDelay = '.' + delayTime + 's'
+          delayTime++
+     })
+}
+
 burgerBtn.addEventListener('click', handleNav)
