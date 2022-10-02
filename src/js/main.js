1451fefa28ead37f772e66d50e15c3facf147a4a
Sławomir <lilcrunker@gmail.com>
2022-10-02 18:34:27 +0200

    in JS: added year changing and burgerBtn changing colors depends of section

--- src/js/main.js
+++ src/js/main.js
@@ -1,26 +1,46 @@
-const nav = document.querySelector('.nav')
-const burgerBtn = document.querySelector('.burger-btn')
-const navItems = document.querySelectorAll('.nav__item')
-const navItemsAnimaton = document.querySelector('.nav-items-animaton')
+const nav = document.querySelector('.nav');
+const burgerBtn = document.querySelector('.burger-btn');
+const navItems = document.querySelectorAll('.nav__item');
+const navItemsAnimaton = document.querySelector('.nav-items-animaton');
+const navBtnBars = document.querySelector('.burger-btn__bars');
+const allSections = document.querySelectorAll('.section');
+const footerYear = document.querySelector('.footer__year');
 
 const handleNav = () => {
-	nav.classList.toggle('nav--active') 
-     
-     navItems.forEach((item)=>{
-          item.addEventListener('click', ()=>{
-             nav.classList.remove ('nav--active')
-          })
+	nav.classList.toggle('nav--active');
+     navBtnBars.classList.remove('black-bars-color')
+ 
+	navItems.forEach((item) => {
+		item.addEventListener('click', () => {
+			nav.classList.remove('nav--active');
+		});
+	});
+	handleNavItemsAnimation();
+};
+const handleNavItemsAnimation = () => {
+	let delayTime = 0;
+	navItems.forEach((item) => {
+		item.classList.toggle('nav-items-animaton');
+		item.style.animationDelay = '.' + delayTime + 's';
+		delayTime++;
+	});
+};
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear();
+	footerYear.innerText = year;
+};
+const handleObserver = () => {
+	const currentSection = window.scrollY;
+     allSections.forEach(section => {
+          if(section.classList.contains('white-section')&& 
+          section.offsetTop <= currentSection+60){
+               navBtnBars.classList.add('black-bars-color')
+          } else if (!section.classList.contains('white-section')&& 
+          section.offsetTop <= currentSection+60){
+               navBtnBars.classList.remove('black-bars-color')
+          }
      })
-     handleNavItemsAnimation()
-}
-const handleNavItemsAnimation=()=>{
-     let delayTime = 0;
-
-     navItems.forEach(item =>{
-          item.classList.toggle('nav-items-animaton')
-          item.style.animationDelay = '.' + delayTime + 's'
-          delayTime++
-     })
-}
-
-burgerBtn.addEventListener('click', handleNav)
+};
+handleCurrentYear();
+burgerBtn.addEventListener('click', handleNav);
+window.addEventListener('scroll', handleObserver)
